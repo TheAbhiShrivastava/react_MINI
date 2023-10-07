6278ea5475cfd319d607862c921143c5ae2f0e11
TheAbhiShrivastava <ak2545307@gmail.com>
2023-10-07 18:16:10 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-11/12 border shadow-md fixed top-0 background  -my-2 '>
+     <header className='flex flex-col justify-between ' >
+        
+        <h1 className='text-4xl font-bold uppercase items-center mx-auto'>ABhi Blog   
+        <img src="https://www.mediwales.com/wp-content/uploads/2013/07/abhi-logo.png" className='w-[80px] h-[80px] items-end mx-auto '></img></h1>
+    
+           
+      </header>
+    </div>
+  )
+}
+
+export default Header
